6554db661a9f7838628ef18a767a9810157a8bcc
Martijn Heil <m.heil375@gmail.com>
2019-04-10 11:49:27 +0000

    Add new file

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+use std::net::TcpStream;
+use std::net::ToSocketAddrs;
+use std::thread::sleep;
+use std::time::Duration;
+
+pub fn knock(to: &[&ToSocketAddrs], delay: u64) {
+  for addr in to {
+    TcpStream::connect(addr);
+    sleep(Duration::from_secs(5));
+  }
+}
