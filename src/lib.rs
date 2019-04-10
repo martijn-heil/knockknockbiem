9cc161a12674a2cdd53cd53cbbc00098ea906072
Martijn Heil <m.heil375@gmail.com>
2019-04-10 12:12:56 +0000

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use std::net::ToSocketAddrs;
 use std::thread::sleep;
 use std::time::Duration;
 
-pub fn knock(to: &[&ToSocketAddrs], delay: u64) {
+pub fn knock<A>(to: A, delay: u64) where A: IntoIterator, A::Item : ToSocketAddrs {
   for addr in to {
     TcpStream::connect(addr);
     sleep(Duration::from_secs(delay));
